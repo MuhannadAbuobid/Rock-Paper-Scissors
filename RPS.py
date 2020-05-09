d52699623a9ef97cafc2d6a2229f9f4875b49f15
Khalid Alomari <khalido394@gmail.com>
2020-05-09 12:57:44 -0700

    Add the random library and its class

--- RPS.py
+++ RPS.py
@@ -1,4 +1,18 @@
 
+import random
 moves  = ['rock', 'paper', 'scissors']
 
-print(moves)
+class player():
+    # """docstring for Player."""
+
+    def __init__(self):
+        # super(Player, self).__init__()
+        self.initalScore = Random.start(self)
+
+
+class Random(player):
+    def start():
+        return random.choice(moves)
+
+test = Random.start()
+print (test)
